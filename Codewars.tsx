d19bb442fe284489839ced632c6e84b083b33e62
TichonovRoman <romikt@inbox.ru>
2022-01-05 23:03:41 +0300

    +1 codewars

--- Codewars.tsx
+++ Codewars.tsx
@@ -19,3 +19,16 @@ function updateLight(current) {
     else if (current==="red") {return "green"}
 
 }
+
+<>Well of Ideas - Easy Version</>
+function well(x) {
+    switch (x.filter(i => i === 'good').length) {
+        case 0:
+            return 'Fail!'
+        case 1:
+        case 2:
+            return 'Publish!'
+        default:
+            return 'I smell a series!'
+    }
+}
